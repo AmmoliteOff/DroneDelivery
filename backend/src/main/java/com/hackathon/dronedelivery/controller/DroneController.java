cd368ffef479b49932ab49a1aca4f0a117ce5e1c
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-26 17:47:47 +0300

    added methods of getting in controllers

--- backend/src/main/java/com/hackathon/dronedelivery/controller/DroneController.java
+++ backend/src/main/java/com/hackathon/dronedelivery/controller/DroneController.java
@@ -1,4 +1,25 @@
 package com.hackathon.dronedelivery.controller;
 
+import com.hackathon.dronedelivery.model.Drone;
+
+import com.hackathon.dronedelivery.service.DroneService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api")
 public class DroneController {
+
+    private final DroneService droneService;
+
+    @PostMapping("/drones")
+    public ResponseEntity<List<Drone>> getDrones() {
+        return ResponseEntity.ok(droneService.findAll());
+    }
 }
--- backend/src/main/java/com/hackathon/dronedelivery/controller/OrderController.java
+++ backend/src/main/java/com/hackathon/dronedelivery/controller/OrderController.java
@@ -1,4 +1,31 @@
 package com.hackathon.dronedelivery.controller;
 
+import com.hackathon.dronedelivery.model.Notification;
+import com.hackathon.dronedelivery.model.Order;
+import com.hackathon.dronedelivery.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/acceptOrder")
+    public Notification acceptOrder(@RequestBody Order order) {
+        return new Notification();
+    }
+
+    public ResponseEntity<List<Order>> getOrders() {
+        return ResponseEntity.ok(orderService.findAll());
+    }
 }
