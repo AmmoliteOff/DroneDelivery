362d8a58dd5e446edbc830964ae0c0d4cab117fa
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-28 02:48:54 +0300

    added methods

--- backend/src/main/java/com/hackathon/dronedelivery/service/UserService.java
+++ backend/src/main/java/com/hackathon/dronedelivery/service/UserService.java
@@ -5,6 +5,7 @@ import com.hackathon.dronedelivery.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -29,5 +30,12 @@ public class UserService {
 
     public void Update(User user){
         userRepository.save(user);
+    }
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 }
