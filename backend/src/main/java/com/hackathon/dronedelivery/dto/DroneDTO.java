4ded8108986a4fc47f621625d874dfe9b163016c
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-28 13:16:31 +0300

    added DroneDTO

---
+++ backend/src/main/java/com/hackathon/dronedelivery/dto/DroneDTO.java
@@ -0,0 +1,12 @@
+package com.hackathon.dronedelivery.dto;
+
+import lombok.Data;
+
+@Data
+public class DroneDTO {
+    private double charge;
+
+    private double fullChargeDistance;
+
+    private double maxWeight;
+}
