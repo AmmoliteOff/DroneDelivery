95e9081c84498cd726f12b81465e9d23adf78990
xtansy <xtansy@mail.ru>
2023-10-28 15:37:00 +0300

    fix

--- frontend/src/widgets/header/ui/Header.tsx
+++ frontend/src/widgets/header/ui/Header.tsx
@@ -2,15 +2,20 @@ import css from "./Header.module.css";
 
 import { useNavigate } from "react-router-dom";
 
-import { Profile } from "entities/user";
+import { useAppSelector } from "shared/model";
+
+import { Profile, userSelector } from "entities/user";
 import { LogoutButton } from "features/logout";
 
 export const Header = () => {
+    const user = useAppSelector(userSelector);
     const navigate = useNavigate();
 
     const onClickLogo = () => {
         navigate("/home");
     };
+
+    if (!user) return null;
     return (
         <div className={css.header}>
             <h1 onClick={onClickLogo} className={css.logo}>
@@ -20,8 +25,8 @@ export const Header = () => {
             <div className={css.actions}>
                 <Profile
                     logout={<LogoutButton />}
-                    name={"Вася"}
-                    surname={"Иванов"}
+                    name={user?.name}
+                    surname={user?.surname}
                     img="https://klike.net/uploads/posts/2023-01/1674365337_3-31.jpg"
                 />
             </div>
