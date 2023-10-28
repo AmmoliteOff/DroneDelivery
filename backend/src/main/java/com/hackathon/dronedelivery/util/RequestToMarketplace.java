6007bbcc08ee37a5ff54771639cfd690eaa6ee40
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-28 04:04:39 +0300

    Replacing UpdateStatus with UpdateOrder

--- backend/src/main/java/com/hackathon/dronedelivery/util/RequestToMarketplace.java
+++ backend/src/main/java/com/hackathon/dronedelivery/util/RequestToMarketplace.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public class RequestToMarketplace {
-    public static void updateStatus(Order order) {
+    public static void updateOrder(Order order) {
         RestTemplate restTemplate = new RestTemplate();
 
         String url = "https://marketplace.com/api/order";
