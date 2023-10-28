770ce6927b932b7f65c7e28a2602a8076371eea9
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-28 04:05:07 +0300

    minor changes

--- backend/src/main/java/com/hackathon/dronedelivery/repository/RequestRepository.java
+++ backend/src/main/java/com/hackathon/dronedelivery/repository/RequestRepository.java
@@ -1,10 +1,9 @@
 package com.hackathon.dronedelivery.repository;
 
-import com.hackathon.dronedelivery.model.DroneSendRequest;
-import com.hackathon.dronedelivery.model.Order;
+import com.hackathon.dronedelivery.model.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RequestRepository  extends JpaRepository<DroneSendRequest, Long> {
+public interface RequestRepository  extends JpaRepository<Request, Long> {
 }
