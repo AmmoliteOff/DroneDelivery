b913396384a2fd1b07315894db74af85a3be2aec
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-28 05:00:30 +0300

    added cors config

--- backend/src/main/java/com/hackathon/dronedelivery/config/SecurityConfig.java
+++ backend/src/main/java/com/hackathon/dronedelivery/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.hackathon.dronedelivery.config;
 
 import com.hackathon.dronedelivery.filter.JwtFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -10,19 +11,43 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
     private final JwtFilter jwtFilter;
     private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new
+                UrlBasedCorsConfigurationSource();
+        var configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin(frontendUrl);
+        configuration.addAllowedHeader("Access-Control-Request-Headers");
+        configuration.addAllowedHeader("Content-Type");
+        configuration.addAllowedHeader("Access-Control-Request-Method");
+        configuration.setAllowedMethods(Arrays.asList( "GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf().disable()
-                .cors().disable()
+                .cors()
+                .and()
                 .authorizeHttpRequests()
                 .requestMatchers("/api/register", "/api/authenticate",  "/api/acceptOrder")
                 .permitAll()
