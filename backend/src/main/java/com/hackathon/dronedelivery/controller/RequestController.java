d9e32c5f909c40a8755b73dfdce183eb9ea6079d
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-28 12:48:06 +0300

    minor change

--- backend/src/main/java/com/hackathon/dronedelivery/controller/RequestController.java
+++ backend/src/main/java/com/hackathon/dronedelivery/controller/RequestController.java
@@ -5,12 +5,14 @@ import com.hackathon.dronedelivery.service.RequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class RequestController {
 
     private final RequestService requestService;
